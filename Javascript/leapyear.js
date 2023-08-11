91197d4885c9ad996d73f84be1298c8783c3bc7f
SmitMachhi <thesmitmachhi@gmail.com>
2023-08-11 16:02:14 -0400

    fizzbuzz

--- Javascript/leapyear.js
+++ Javascript/leapyear.js
@@ -1,11 +1,11 @@
 function isLeap(year) {
 
-    if (year % 4 == 0) {
-        if (year % 100 != 0) {
+    if (year % 4 === 0) {
+        if (year % 100 !== 0) {
             console.log("Leap year.");
         }
         else {
-            if (year % 400 == 0) {
+            if (year % 400 === 0) {
                 console.log("Leap year.");
             }
             else {
