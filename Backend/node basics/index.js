6dc9b138fe8215c40c05881316d5f6de88b550ea
SmitMachhi <thesmitmachhi@gmail.com>
2023-09-23 08:59:11 -0400

    node basics

--- Backend/node basics/index.js
+++ Backend/node basics/index.js
@@ -4,3 +4,7 @@ fs.writeFile("message.txt", "Wasssssup Node!", (err) => {
     if (err) throw err;
     console.log("file has been saved!");
 });
+
+fs.readFile('message.txt', 'utf8', function (err, dataBro) {
+    console.log(dataBro);
+});
